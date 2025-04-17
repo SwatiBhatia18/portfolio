8a6af0d4ea6ea55d7c4bdd2a40e5cbb103ad2cab
Swati Bhatia <swatibhatia626@gmail.com>
2025-04-17 12:49:05 +0530

    Added home, about and projects section

---
+++ components/about.tsx
@@ -0,0 +1,43 @@
+"use client"
+import React from 'react'
+import { motion } from 'framer-motion'
+import SectionHeading from './section-heading'
+import { useSectionInView } from '@/lib/hooks'
+
+
+export default function About() {
+  const {ref} = useSectionInView("About")
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        As a <span className="font-medium">Software Engineer</span> with{" "}
+        <span className="font-medium">3 years of experience</span>, I specialize in{" "}
+        <span className="font-medium">Frontend Development</span>.{" "}
+        <span className="italic">What I love most about programming</span> is
+        the problem-solving aspect—that <span className="underline">thrill</span>{" "}
+        of finally cracking a tough challenge. My expertise lies in{" "}
+        <span className="font-medium">
+          React, Next.js, JavaScript, HTML, and CSS
+        </span>
+        , with proficiency in TypeScript. I'm passionate about{" "}
+        <span className="font-medium">building scalable, performant applications</span>{" "}
+        while continuously expanding my technical horizons.
+      </p>
+
+      <p>
+        <span className="italic">Beyond coding</span>, I enjoy traveling, watching movies, and{" "}
+        <span className="font-medium">continuous learning</span>. Currently, I'm{" "}
+        <span className="font-medium">deepening my knowledge</span> of system design principles
+        to enhance my architectural thinking.
+      </p>
+    </motion.section>
+  )
+}
---
+++ lib/hooks.ts
@@ -0,0 +1,19 @@
+"use client"
+
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { SectionName } from "./type";
+import { useActiveSectionContext } from "@/context/active-section-context";
+
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+  const { ref, inView } = useInView({ threshold });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection(sectionName);
+    }
+  }, [inView, setActiveSection, timeOfLastClick, sectionName]);
+
+  return { ref };
+}
